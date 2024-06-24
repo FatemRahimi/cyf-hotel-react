5d7e4ca8514daee3ede696e3baa287350d479dea
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2024-06-24 17:18:20 +0100

    Commit before ejecting

--- src/components/destinations/Destinations.jsx
+++ src/components/destinations/Destinations.jsx
@@ -14,11 +14,15 @@ function Destinations() {
         <h1>All-Inclusive Resorts</h1>
         <p>On the Caribbean's Best Beaches</p>
         <div className="img-container">
-          <img className="span-3 image-grid-row-2" src={BoraBora} alt="/" />
-          <img src={BoraBora2} alt="/" />
-          <img src={Maldives} alt="/" />
-          <img src={Maldives2} alt="/" />
-          <img src={KeyWest} alt="/" />
+          <img
+            className="span-3 image-grid-row-2"
+            src={BoraBora}
+            alt="Bora Bora"
+          />
+          <img src={BoraBora2} alt="Bora Bora 2" />
+          <img src={Maldives} alt="Maldives" />
+          <img src={Maldives2} alt="Maldives 2" />
+          <img src={KeyWest} alt="Key West" />
         </div>
       </div>
     </div>
